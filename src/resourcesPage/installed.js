3ded99901c485e22ff7456d90f78121fdbc074c3
Cyclip <gdneop@gmail.com>
2022-05-31 16:40:52 +0100

    Add loadPacks() structured function

--- src/resourcesPage/installed.js
+++ src/resourcesPage/installed.js
@@ -12,8 +12,44 @@ export class InstalledTab extends Component {
         super(props);
 
         this.state = {
+            /* list of all installed packs, example:
+                [
+                    {
+                        name: "coloured resource pack name",
+                        desc: "pack description",
+                        filename: "path/to/pack.zip",
+                        tmpImagePath: "path/to/icon.png"
+                    }
+                ]
+
+                All resource packs root files' contents (pack.mcmeta, pack.png)
+                are cached, and the image path is specified as tmpImagePath
+            */
             installedPacks: [],
         };
+    }
+
+    async componentDidMount() {
+        this.loadPacks();
+    }
+
+    loadPacks = async() => {
+        console.warn("no packs loaded");
+        
+        let sample = [];
+        for (let i = 0; i < 10; i++) {
+            sample.push({
+                name: `pack${i}`,
+                desc: "example desc",
+                filename: `pack${i}.zip`,
+                // empty => placeholder
+                tmpImagePath: ``,
+            });
+        }
+
+        this.setState({
+            installedPacks: sample
+        }, () => console.log(this.state));
     }
 
     render() {
